2157525e17ae1f110f7befc6f3ae1971b1d5b8af
banupriya-dotcom <banupriya02.02@gmail.com>
2025-06-21 22:35:18 +0530

    Add files via upload
    day1

---
+++ day1/FireFox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[]args) {
+		// TODO Auto-generated method stub
+
+
+float browserVersion = 100.2f;
+String browserName = "firefox";
+boolean isVisible = true;
+int releaseYear = 1998;
+char browserLogo = 'f';
+System.out.println("The Browser version:"+ browserVersion);
+System.out.println("isVisible:"+ isVisible);
+System.out.println("Release Year:" + releaseYear);
+System.out.println("Browser Logo:" + browserLogo);
+	// TODO Auto-generated method stub
+
+}
+}
